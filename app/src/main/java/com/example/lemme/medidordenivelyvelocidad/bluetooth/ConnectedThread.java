482dec7fcdd5fbe04e3d448e436fad037f139b0c
Luisa Emme <lemme@thoughtworks.com>
2015-06-19 01:27:54 -0500

    refactoring en clase connected thread para separar message handler

--- app/src/main/java/com/example/lemme/medidordenivelyvelocidad/bluetooth/ConnectedThread.java
+++ app/src/main/java/com/example/lemme/medidordenivelyvelocidad/bluetooth/ConnectedThread.java
@@ -42,31 +42,34 @@ public class ConnectedThread extends Thread {
     }
 
     public void initializeHandler() {
-        handler = new Handler() {
-            @Override
-            public void handleMessage(Message msg) {
-                switch (msg.what) {
-                    case Utilities.RECEIVE_MESSAGE:
-                        byte[] readBuffer = (byte[]) msg.obj;
-                        stringBuffer.append(new String(readBuffer, 0, msg.arg1));
-                        int endOfLine = stringBuffer.indexOf("\r\n");
-                        if(endOfLine > 0){
-                            String subString = stringBuffer.substring(0, endOfLine);
-                            stringBuffer.delete(0, stringBuffer.length());
-                            Log.d(Utilities.TAG, subString);
-                            try {
-                                chart.getSerie().addSensorLecture(Float.valueOf(subString));
-                                chart.updateChart();
-                            } catch (Exception ex) {
-                                ex.printStackTrace();
-                            }
-                        }
-                        break;
-                    default:
-                        break;
-                }
-            }
-        };
+        handler = new MessageHandler()
+
+//                new Handler() {
+//            @Override
+//            public void handleMessage(Message msg) {
+//                switch (msg.what) {
+//                    case Utilities.RECEIVE_MESSAGE:
+//                        byte[] readBuffer = (byte[]) msg.obj;
+//
+//                        stringBuffer.append(new String(readBuffer, 0, msg.arg1));
+//                        int endOfLine = stringBuffer.indexOf("\r\n");
+//                        if(endOfLine > 0){
+//                            String subString = stringBuffer.substring(0, endOfLine);
+//                            stringBuffer.delete(0, stringBuffer.length());
+//                            Log.d(Utilities.TAG, subString);
+//                            try {
+//                                chart.getSerie().addSensorLecture(Float.valueOf(subString));
+//                                chart.updateChart();
+//                            } catch (Exception ex) {
+//                                ex.printStackTrace();
+//                            }
+//                        }
+//                        break;
+//                    default:
+//                        break;
+//                }
+//            }
+//        };
     }
 
     @Override
---
+++ app/src/main/java/com/example/lemme/medidordenivelyvelocidad/bluetooth/MessageHandler.java
@@ -0,0 +1,47 @@
+package com.example.lemme.medidordenivelyvelocidad.bluetooth;
+
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
+
+import com.example.lemme.medidordenivelyvelocidad.chart.Chart;
+import com.example.lemme.medidordenivelyvelocidad.commons.Utilities;
+
+/**
+ * Created by lemme on 6/19/15.
+ */
+public class MessageHandler extends Handler {
+    private Chart chart;
+    private StringBuffer stringBuffer;
+
+    public MessageHandler(Chart chart, StringBuffer stringBuffer) {
+        this.chart = chart;
+        this.stringBuffer = stringBuffer;
+    }
+
+    @Override
+    public void handleMessage(Message msg) {
+        switch (msg.what) {
+            case Utilities.RECEIVE_MESSAGE:
+                byte[] readBuffer = (byte[]) msg.obj;
+
+                stringBuffer.append(new String(readBuffer, 0, msg.arg1));
+                int endOfLine = stringBuffer.indexOf("\r\n");
+                if(endOfLine > 0){
+                    String subString = stringBuffer.substring(0, endOfLine);
+                    stringBuffer.delete(0, stringBuffer.length());
+                    Log.d(Utilities.TAG, subString);
+                    try {
+                        chart.getSerie().addSensorLecture(Float.valueOf(subString));
+                        chart.updateChart();
+                    } catch (Exception ex) {
+                        ex.printStackTrace();
+                    }
+                }
+                break;
+            default:
+                break;
+        }
+    }
+    }
+}
